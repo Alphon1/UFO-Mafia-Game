d5e1fbd9e2331930122f4c6a2c3d824cbee3f339
Alex Williamson <alex_williamson@rocketmail.com>
2020-10-06 16:14:40 +0100

    Added a file for Scripted Objects, started coding the attack script

--- Assets/Scripts/Attack_Handling.cs
+++ Assets/Scripts/Attack_Handling.cs
@@ -13,6 +13,26 @@ public class Attack_Handling : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        //sends a ray from the camera to where the mouse clicked
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if the ray hits an enemy, calls the attack
+        if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Enemy")
+        {
+            Friendly_Attack(, hit, );
+        }
+    }
+
+    public void Friendly_Attack(GameObject Current_Character, GameObject Target, float Weapon_Range)
+    {
+        //checks if there's nothing in the way of the attack
+        if(Physics.Linecast(Current_Character.transform.position, Target.transform.position))
+        {
+            //checks if the selected enemy is in range
+            if(Vector3.Distance(Current_Character.transform.position, Target.transform.position) < Weapon_Range)
+            {
+
+            }
+        }
     }
 }
