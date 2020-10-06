a32448772ccdf137748ebb14c7f512b7e2731eb9
Alper Mimoglu <alpermimoglu@gmail.com>
2020-10-06 16:57:21 +0100

    added navmesh

---
+++ Assets/Scenes/Menus/MainMenu/Scipts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   
+    public Camera cam;
+    public UnityEngine.AI.NavMeshAgent agent;
+    public float maxdistance = 20;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (Vector3.Distance(agent.transform.position,hit.point)<=maxdistance) {
+                    agent.SetDestination(hit.point);
+                    Debug.Log("Vaid point");
+
+                }
+                else
+                {
+                    Debug.Log("Invaild point");
+                }
+                //Debug.Log("Hit" + hit.collider.name + "" + hit.point);
+            }
+        }
+    }
+}
