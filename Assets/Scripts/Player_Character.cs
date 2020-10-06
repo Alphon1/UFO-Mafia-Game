317c173b4cd0e779078935ea5421ec34da44b9a0
Alex Williamson <alex_williamson@rocketmail.com>
2020-10-06 16:34:19 +0100

    Added a script to allow creation of Player Characters

---
+++ Assets/Scripts/Player_Character.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Character", menuName = "Character", order = 1)]
+public class Player_Character : ScriptableObject
+{
+    public int Range;
+    public int Max_Health;
+    public int Damage;
+}
