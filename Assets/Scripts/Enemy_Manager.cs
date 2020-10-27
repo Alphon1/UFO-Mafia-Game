c48bf67b536129dcb6abad97be4cb5f38f81b510
Alex Williamson <alex_williamson@rocketmail.com>
2020-10-27 19:32:16 +0000

    Fixed bug where enemies stayed visible after killing one, also whatever alper said in discord

--- Assets/Scripts/Enemy_Manager.cs
+++ Assets/Scripts/Enemy_Manager.cs
@@ -10,6 +10,8 @@ public class Enemy_Manager : MonoBehaviour
     private int initiative;
     public int Current_Health;
     public bool isyourturn;
+    private GameObject Game_Manager;
+
 
     public void End_turn()
     {
@@ -20,16 +22,23 @@ public class Enemy_Manager : MonoBehaviour
     {
         Max_Health = 20;
         Current_Health = Max_Health;
-        
-        
+        Game_Manager = GameObject.FindWithTag("Game_Manager");
+
     }
 
     private void Update()
     {
         if (Current_Health <= 0)
         {
-            Destroy(GameObject.FindWithTag("Enemy"));
+            Game_Manager.GetComponent<Game_Manager>().Visible_Enemies.Remove(gameObject);
+            Game_Manager.GetComponent<Game_Manager>().Enemy_List.Remove(gameObject);
+            foreach (GameObject Player_Char in GameObject.FindGameObjectsWithTag("Tonies"))
+            {
+                Player_Char.GetComponentInChildren<Player_Vision>().Nearby_Enemies.Remove(gameObject);
+            }
+            Destroy(gameObject);
             Debug.Log("Dead");
         }
     }
 }
+
