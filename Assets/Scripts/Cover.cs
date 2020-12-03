d91f3ea7c9f86ea72e93a8a8eee719b564ce2c59
giblets2k <giblets2k@gmail.com>
2020-12-03 14:18:59 +0000

    Added cover Scripts/Cover.cs to detect whether a entity is in cover.

---
+++ Assets/Scripts/Cover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cover : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Cover")
+        {
+            other.tag = "!Cover";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "!Cover")
+        {
+            other.tag = "Cover";
+        }
+    }
+}
