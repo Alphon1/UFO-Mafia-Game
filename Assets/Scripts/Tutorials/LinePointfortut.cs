5300771a9b082683773d678405cd2eb86c73a3b4
AlperMimoglu <alpermimoglu@gmail.com>
2021-05-04 15:36:35 +0100

    fixed the ui
    Signed-off-by: AlperMimoglu <alpermimoglu@gmail.com>

---
+++ Assets/Scripts/Tutorials/LinePointfortut.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinePointfortut : MonoBehaviour
+{
+    [SerializeField] public Transform[] points;
+    [SerializeField] private LineController line;
+
+    public Game_Managerfortut gameM;
+    public GameObject enemyTarget;
+
+    private void Start()
+    {
+        line.SetUpLine(points);
+    }
+
+    private void Update()
+    {
+        points[0] = gameM.Current_Char.transform;
+        points[1] = enemyTarget.transform;
+    }
+}
