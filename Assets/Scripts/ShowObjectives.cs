e7c0bdbf170d1ccc78d1c7ca10f4110bbf52791d
Alphon1 <alex_williamson@rocketmail.com>
2021-05-14 13:38:42 +0100

    Added script to show/hide objectives

---
+++ Assets/Scripts/ShowObjectives.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowObjectives : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Expanded_Objectives;
+    [SerializeField]
+    private GameObject Minimized_Objectives;
+
+    // Start is called before the first frame update
+    void Toggle_Objectives()
+    {
+        if(Expanded_Objectives.activeSelf)
+        {
+            Expanded_Objectives.SetActive(false);
+            Minimized_Objectives.SetActive(true);
+        }
+        else
+        {
+            Expanded_Objectives.SetActive(true);
+            Minimized_Objectives.SetActive(false);
+        }
+    }
+}
